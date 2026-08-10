e229235ba400e13f972b2bfe79968ad52eb54147
suckerson <tjrgus1849@naver.com>
2026-08-10 17:12:47 +0900

    feat: 친추기능 (요청, 수락, 조회 순서로)

---
+++ src/main/java/son/suck/muzik/dto/FriendRequestDto.java
@@ -0,0 +1,10 @@
+package son.suck.muzik.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class FriendRequestDto {
+    private String targetNickname;
+}
