8651f045a26d1458d2f3f0aed28feba699a1fe5f
suckerson <tjrgus1849@naver.com>
2026-08-27 15:15:31 +0900

    feat: jwt config

--- src/main/java/son/suck/muzik/config/SecurityConfig.java
+++ src/main/java/son/suck/muzik/config/SecurityConfig.java
@@ -1,32 +1,48 @@
 package son.suck.muzik.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()) // html, css, js, images 등
+                .requestMatchers("/", "/*.html", "/favicon.ico");
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                // REST API 서버 특성 설정 (세션 미사용, CSRF 및 기본 폼 로그인 비활성화)
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .authorizeHttpRequests(auth -> auth
+                        // 로그인, 회원가입 등 비인증 사용자용 API
+                        .requestMatchers("/api/users/signup", "/api/users/login").permitAll()
+
+                        .anyRequest().authenticated()
                 )
 
-                // 모든 URL 접근 허용
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                // JWT 인증 필터배치
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
