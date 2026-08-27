89cd209fbb8fdd9c26b20ac676307442e18cddc7
suckerson <tjrgus1849@naver.com>
2026-08-27 17:08:32 +0900

    fix: gamemenu logout fix

--- src/main/resources/static/gameMenu.js
+++ src/main/resources/static/gameMenu.js
@@ -2,9 +2,13 @@ function initGameMenuPage() {
     const nickname = localStorage.getItem('nickname') || '플레이어';
 }
 
-function handleLogout() {
-    localStorage.removeItem('userId');
-    localStorage.removeItem('nickname');
+function logout() {
+    localStorage.clear();
     alert('로그아웃 되었습니다.');
-    navigateTo('home');
+
+    if (typeof navigateTo === 'function') {
+        navigateTo('home');
+    } else {
+        window.location.href = 'index.html';
+    }
 }
